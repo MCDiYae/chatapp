7fa1ab42de3e3ab9606e9ccd3e4866296f49f814
MCDiYae <mohammeddiyae.chbalou@etu.uae.ac.ma>
2023-02-07 14:12:01 +0100

    initialization Firebase

--- lib/pages/registrepage.dart
+++ lib/pages/registrepage.dart
@@ -14,49 +14,51 @@ class RegitrePage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 30),
-        child: Column(
-          children: [
-            Image.asset('assets/images/scholar.png'),
-            Texto(title: title1),
-            const SizedBox(
-              height: 64,
-            ),
-            Row(
-              children: [
-                Texto(title: sign),
-              ],
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            const TextFieldForm(
-              hinttexte: 'Email',
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            const TextFieldForm(
-              hinttexte: 'Password',
-            ),
-            const SizedBox(
-              height: 64,
-            ),
-            const CostumBotton(
-              name: 'SignUP',
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: RichTextLogin(
-                keyy: keySign,
-                value: valueSign,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset('assets/images/scholar.png'),
+              Texto(title: title1),
+              const SizedBox(
+                height: 64,
               ),
-            ),
-          ],
+              Row(
+                children: [
+                  Texto(title: sign),
+                ],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const TextFieldForm(
+                hinttexte: 'Email',
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const TextFieldForm(
+                hinttexte: 'Password',
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              const CostumBotton(
+                name: 'SignUP',
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: RichTextLogin(
+                  keyy: keySign,
+                  value: valueSign,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
