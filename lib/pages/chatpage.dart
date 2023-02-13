e9b05da01a6044528295f450fc295e8028343837
MCDiYae <mohammeddiyae.chbalou@etu.uae.ac.ma>
2023-02-13 10:16:52 +0100

    future builder

--- lib/pages/chatpage.dart
+++ lib/pages/chatpage.dart
@@ -8,42 +8,51 @@ import '../widget/chatmessage.dart';
 
 class ChatPage extends StatelessWidget {
   static String id = 'chatPage';
+  final TextEditingController _controller = TextEditingController();
+
   CollectionReference message =
-      FirebaseFirestore.instance.collection('message');
+      FirebaseFirestore.instance.collection(keyMessage);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('CHATT'),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return Chatmessage();
-              },
-            ),
+    return FutureBuilder<DocumentSnapshot>(
+      future: message.doc('d7cgTzRATSDS78CvHQRc').get(),// read 
+      builder: (context, snapshot) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('CHATT'),
+            centerTitle: true,
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              onSubmitted: (data) {
-                message.add({
-                  'message': data,
-                });
-              },
-              decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6),
-                      borderSide: BorderSide(color: colorWrite)),
-                  suffixIcon: Icon(Icons.send)),
-            ),
+          body: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Chatmessage();
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: _controller,
+                  onSubmitted: (data) {
+                    message.add({
+                      'message': data,
+                    });
+                    _controller.clear();
+                  },
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                          borderSide: BorderSide(color: colorWrite)),
+                      suffixIcon: Icon(Icons.send)),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
